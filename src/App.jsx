3f479526e47fcabc0dba21aba11d97cef9182db7
Abhikhush28 <sahuabhikhush@gmail.com>
2024-02-01 13:01:04 +0530

    again bannerHome updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-full'>
+    <div className=''>
     {/* <NavBar/> */}
       {/* <Home/> */}
       {/* <Login/> */}
